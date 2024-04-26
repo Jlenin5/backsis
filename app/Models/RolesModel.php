2f4651a5dce76f117deacf4da5e863def6346852
Jlenin5 <jlenin25mv@gmail.com>
2024-04-26 11:43:28 -0500

    model

--- app/Models/RolesModel.php
+++ app/Models/RolesModel.php
@@ -6,13 +6,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class RolesModel extends Model {
-    protected $table = 'Roles';
-    protected $primaryKey = 'id';
+
+    protected $table = 'roles';
     public $timestamps = false;
     use HasFactory;
 
     protected $fillable = [
         'id',
-        'rolName',
+        'name',
     ];
 }   
