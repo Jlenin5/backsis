89a294c2427da205db98ccdc1a75d29983482b58
Jlenin5 <jlenin25mv@gmail.com>
2024-04-26 10:33:33 -0500

    usersDB

--- routes/api.php
+++ routes/api.php
@@ -40,9 +40,9 @@ use App\Http\Controllers\WorkAreaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::controller(AvatarsController::class)->group(function() {
     Route::get('/ava', 'index');
